5d6a988bc883e6e302fca9856e899799d00975af
rehan <rehans516@gmail.com>
2016-04-06 14:06:28 +0200

    continuous distance measure

--- range_sensor_loop.py
+++ range_sensor_loop.py
@@ -1,24 +1,49 @@
 import RPi.GPIO as GPIO
 import time
+import paho.mqtt.client as mqtt
+
 GPIO.setmode(GPIO.BCM)
+
 TRIG = 23 
 ECHO = 24
+
 print "Distance Measurement In Progress"
+
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
+
 GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
 time.sleep(2)
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
-for i in range (0,15):
+
+mqttc = mqtt.Client("python_pub")
+mqttc.connect("127.0.0.1", 1883)
+
+def publish(dist):
+	mqttc.publish("pi/distance",dist)
+
+def measure():
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+
 	while GPIO.input(ECHO)==0:
- 		pulse_start = time.time()
+	  	pulse_start = time.time()
+
 	while GPIO.input(ECHO)==1:
-		pulse_end = time.time()
+  		pulse_end = time.time()
+
 	pulse_duration = pulse_end - pulse_start
+
 	distance = pulse_duration * 17150
+
 	distance = round(distance, 2)
+	
+	publish(distance)
 	print "Distance:",distance,"cm"
+
+while True:
+	measure()	
+	time.sleep(5)
+
 GPIO.cleanup()
