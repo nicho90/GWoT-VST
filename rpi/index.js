54d1530d59b356bf0d477123072ef37725372bdc
Heinrich Löwen <loewen.heinrich@uni-muenster.de>
2016-04-21 11:54:39 +0200

    add mqtt-client test functionality to pi node script

--- rpi/index.js
+++ rpi/index.js
@@ -2,29 +2,41 @@
 var usonic = require("r-pi-usonic");
 var mqtt = require("mqtt");
 
-// Global measurement variables
+
+/**
+ * Pysical connection to the sensor
+ */
 var gpio = {
-  trig : 23,
-  echo : 24,
-  measurementTimeout : 750
+  trig : 23,  // TRIG pin of the sensor; fix
+  echo : 24,  // ECHO pin of the sensor; fix
+  measurementTimeout : 750  // timeout for the r-pi-usonic package; fix
+  sensor : null;  // measuring function
 }
 
+
+/**
+ * Sensor Data
+ */
 var sensor = {
-  sensor : null,
-  sensor_id: "rpi-1",
-  lng: 7.698035, // e.g. from GPS-Sensor or Settings
-  lat: 51.9733937, // e.g. from GPS-Sensor or Settings
-  interval: 1000, // ms => 1 min = 60000 ms
-  distance: 100 // reference hight of the sensor
+  id : "rpi-1",
+  lng : 7.698035, // e.g. from GPS-Sensor or Settings
+  lat : 51.9733937, // e.g. from GPS-Sensor or Settings
+  interval : 1000, // ms => 1 min = 60000 ms
+  distance : 100 // reference hight of the sensor
 }
 
+
+/**
+ * Measurement object for storing every measurment and generating the message
+ */
 var measurement = {
-  sensor_id : sensor.sensor_id,
+  id : sensor.id,
   timestamp : new Date(),
   measurement : 0,  // Distance in cm
-  lng: sensor.lng, // (regarding geoMQTT)
-  lat: sensor.lat // (regarding geoMQTT)
+  lng : sensor.lng, // (regarding geoMQTT)
+  lat : sensor.lat // (regarding geoMQTT)
 }
+
 
 /**
  * Ultasonic Sensor initialization. Needs to be called once when script starts
@@ -35,11 +47,12 @@ var initSensor = function() {
       console.log("Sensor initialization failed.");
     } else {
       console.log("Sensor initialization succeeded. " + new Date());
-      sensor.sensor = usonic.createSensor(gpio.echo, gpio.trig, gpio.measurementTimeout);
+      gpio.sensor = usonic.createSensor(gpio.echo, gpio.trig, gpio.measurementTimeout);
       timer.start();
     }
   });
 }
+
 
 /**
  *
@@ -76,9 +89,73 @@ var timer = {
   }
 };
 
+
+/**
+ * Function to set a new timer interval
+ */
 var setTimerInterval = function(iv) {
   timer.stop();
   timer.start(iv);
 };
 
+
 initSensor();
+
+
+/**
+ * Create MQTT-Client and setup clientId, if MQTT-Broker is online (heartbeat)
+ */
+var client = mqtt.connect('mqtt://127.0.0.1:1883', {
+    encoding : 'utf8',
+    clientId : 'rpi',
+    will : { // Last Will (if Sensor goes offline)
+        topic : 'dead',
+        payload : 'mypayload',
+        qos : 2,
+        retain : true
+    }
+});
+
+
+/**
+ * Subscribe to topic from MQTT-Broker
+ */
+client.subscribe('cloud');
+// TODO: Define MQTT-Messages
+
+
+/**
+ * Connect to MQTT-Broker
+ */
+client.on('connect', function () {
+  // TEST: Send a message every 3 Seconds to Broker
+  var i = 0;
+  setInterval(function() {
+    var topic = '/rpi/test';
+    var message = measurement.toString();
+    var options = {
+      qos : 2, // Quality of Service: 2 = at least once
+      retain : false
+    };
+
+    // Publish message
+    client.publish(topic, message, options, function(){
+      console.log("rpi: Hello from rpi")
+    });
+    i += 1;
+  }, 3000);
+});
+
+
+/**
+ * Recieve Messages from MQTT-Broker
+ */
+client.on('message', function (topic, message) {
+  console.log(topic + ": " + message.toString());
+  //client.end(); // No need to close connection, otherwise program will be closed
+
+  // TODO:
+  // - Implement Settings (Topic) from the MQTT-Broker
+  // - Implement update-interval, if defined thresholds are closer
+  // - Implement update-interval for real-time-data, if a User clicks in the WebClient on a Sensor
+});
