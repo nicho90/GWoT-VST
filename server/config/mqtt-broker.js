6284bbccab278a402ffecafb4931e7d5f5b1d254
Nicho <nicho90@web.de>
2016-04-23 12:28:57 +0200

    Implemented MQTT-Broker

---
+++ server/config/mqtt-broker.js
@@ -0,0 +1,60 @@
+/**
+ * Set Database Option for Pub/Sub Collection (Redis or MongoDB)
+ * Create MQTT-Broker, if no Broker was needed, start only the simple webserver
+ */
+var mosca = require('mosca');
+var pubSubOption = require('../server.js').pubSubOption;
+var simpleServer = false;
+var broker;
+
+
+// Set Mosca-Broker settings
+var moscaSettings = {
+    port: 1883
+};
+
+// Set more specific Mosca-Broker settings
+if (pubSubOption === "redis") {
+
+    // Use Redis
+    var ascoltatore = {
+        type: 'redis',
+        redis: require('redis'),
+        db: 12,
+        port: 6379,
+        return_buffers: true, // to handle binary payloads
+        host: "localhost"
+    };
+    moscaSettings.backend = ascoltatore;
+    moscaSettings.persistence = { factory: mosca.persistence.Redis };
+
+} else if (pubSubOption === "mongodb") {
+
+    // Use MongoDB
+    var ascoltatore = {
+        type: 'mongo',
+        url: 'mongodb://127.0.0.1:27017/mqtt',
+        pubsubCollection: 'ascoltatori',
+        mongo: {}
+    };
+    moscaSettings.backend = ascoltatore;
+    moscaSettings.persistence = {
+        factory: mosca.persistence.Mongo,
+        url: 'mongodb://127.0.0.1:27017/mqtt'
+    };
+
+} else {
+    simpleServer = true;
+}
+
+
+/**
+ * Create Mosca-Broker
+ */
+if (!simpleServer) {
+    broker = new mosca.Server(moscaSettings);
+} else {
+    console.log("Simple Webserver, no MQTT-Broker");
+}
+
+exports.broker = broker;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,58 @@
+var program = require('commander');
 var path = require('path');
 
 
 /**
- * Express-Webserver
+ * Command-Line params
+ */
+program
+    .version('1.0.0')
+    .option('-d, --dev', 'Use local development database')
+    .option('-m, --mongodb', 'Use MongoDB database')
+    .option('-r, --redis', 'Use Redis database')
+    //.option('-e, --entries', 'Add Example-Entries')
+    //.option('-f, --force', 'Delete all entries')
+    .parse(process.argv);
+
+// Check for local or in-production database
+var devStatus = false;
+if(program.dev){
+    devStatus = true;
+}
+exports.dev = devStatus;
+
+// Check if Redis or MongoDB was selected as pub-sub-database, otherwise run only simple webserver
+var pubSubOption;
+if(program.redis){
+    pubSubOption = "redis";
+} else if (program.mongodb){
+    pubSubOption = "mongodb";
+} else {
+    ubSubOption = "";
+}
+exports.pubSubOption = pubSubOption;
+
+
+
+/**
+ * Start Express-Webserver
  */
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-
 // Set Server-Port
 var port = process.env.PORT || 3000;
 server.listen(port, function () {
-    console.log('Server listening at port %d', port);
+    console.log('Webserver listening at port %d', port);
 });
-
 
 // Set folder for static files (WebClient)
 app.use(express.static(__dirname + '/public'));
+
+
+/**
+ * Start MQTT-Broker
+ */
+var broker = require('./config/mqtt-broker.js').broker;
+var messages = require('./config/mqtt-message-handler');
