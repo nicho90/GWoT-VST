7f4b022ee1254417a5373026e9db35d50d217af4
Timm Kuehnel <t_kueh06@uni-muenster.de>
2016-05-10 16:35:28 +0200

    Started WebClient Implementation

---
+++ server/public/js/app.js
@@ -0,0 +1,21 @@
+var app = angular.module("gwot-vst", [
+
+        // External Modules
+        //"ngRoute",
+        "pascalprecht.translate",
+
+        // Own Modules
+        //"routes"
+        "languages"
+
+    ]
+);
+
+
+// Start App
+app.run(function($translate) {
+
+    // Use Translator and set
+    $translate.use('en_US');
+
+});
