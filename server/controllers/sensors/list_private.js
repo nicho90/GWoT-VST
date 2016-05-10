481ca4e619a1c342af593d083eb3a95e40cbb0a8
Nicho <nicho90@web.de>
2016-05-10 11:03:02 +0200

    Implemented REST-API Endpoint /users/:username/sensors (GET)
    List all private sensor by username, with query ?public=true, the
    results will be extended with the public sensors

---
+++ server/controllers/sensors/list_private.js
@@ -0,0 +1,62 @@
+var pg = require('pg');
+var jwt = require('jsonwebtoken');
+var secret = require('./../../config/secret');
+var verifier = require('./../../config/verifier');
+var db_settings = require('../../server.js').db_settings;
+
+
+// LIST
+exports.request = function(req, res){
+
+	// Decode Token
+	jwt.verify(req.headers.token, secret.key, function(err, decoded) {
+		if (err) {
+			res.status(401).json({
+				message: 'Failed to authenticate with this token'
+			});
+        } else {
+			// Further token valiation
+			var validation = verifier(decoded, req.params.username);
+			console.log(validation);
+
+			if(!validation.success) {
+				res.status(401).json({
+					message: validation.message
+				});
+			} else {
+
+				// Create URL
+				var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+
+				// Connect to Database
+				pg.connect(url, function(err, client, done) {
+					if(err) {
+						console.error('Error fetching client from pool', err);
+					} else {
+
+						// Check if only private sensors were requested
+						var query;
+						if(req.query.public) {
+							query = "SELECT DISTINCT * FROM (SELECT created_by, sensor_id, device_id, description, private, sensor_height, ST_X(coordinates) AS lng, ST_Y(coordinates) AS lat, created, updated FROM Sensors WHERE private=false UNION ALL SELECT created_by, sensor_id, device_id, description, private, sensor_height, ST_X(coordinates) AS lng, ST_Y(coordinates) AS lat, created, updated FROM Sensors WHERE created_by='nicho90') AS Sensors";
+						} else {
+							query = "SELECT created_by, sensor_id, device_id, description, private, sensor_height, ST_X(coordinates) AS lng, ST_Y(coordinates) AS lat, created, updated FROM Sensors WHERE created_by='nicho90'";
+						}
+
+						// Database Query
+						client.query(query, function(err, result) {
+							done();
+
+							if(err) {
+								return console.error('error running query', err);
+							} else {
+
+								// Send Result
+								res.status(200).send(result.rows);
+							}
+						});
+					}
+				});
+			}
+		}
+	});
+};
