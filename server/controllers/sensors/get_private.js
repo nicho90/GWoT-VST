bd5b769d9e1344feb6459b0473a3a847049ac27f
Nicho <nicho90@web.de>
2016-05-12 23:39:30 +0200

    Implemented REST-API Endpoint /users/:username/sensors/:sensor_id (GET)
    Get all details of a public or private sensor, which was created by the
    logged-in user

---
+++ server/controllers/sensors/get_private.js
@@ -0,0 +1,72 @@
+var pg = require('pg');
+var types = require('pg').types;
+types.setTypeParser(1700, 'text', parseFloat);
+var jwt = require('jsonwebtoken');
+var secret = require('./../../config/secret');
+var verifier = require('./../../config/verifier');
+var db_settings = require('../../server.js').db_settings;
+
+
+// GET (PRIVATE)
+exports.request = function(req, res){
+
+	// Decode Token
+	jwt.verify(req.headers.token, secret.key, function(err, decoded) {
+		if (err) {
+			res.status(401).json({
+				message: 'Failed to authenticate with this token'
+			});
+        } else {
+			// Further token valiation
+			var validation = verifier(decoded, req.params.username);
+			console.log(validation);
+
+			if(!validation.success) {
+				res.status(401).json({
+					message: validation.message
+				});
+			} else {
+
+				// Create URL
+				var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+
+				// Connect to Database
+				pg.connect(url, function(err, client, done) {
+					if(err) {
+						console.error('Error fetching client from pool', err);
+					} else {
+
+						// Database Query
+						client.query('SELECT sensor_id, device_id, description, private, sensor_height, ST_X(coordinates) AS lng, ST_Y(coordinates) AS lat, created, updated FROM Sensors WHERE created_by=$1 AND sensor_id=$2;', [
+							req.params.username,
+							req.params.sensor_id
+						], function(err, result) {
+							done();
+
+							if(err) {
+								console.error('Error running query: ', err);
+								res.status(401).json({
+									message: 'Error running query',
+									error: err
+								});
+								return;
+							} else {
+
+								// Check if sensor exist
+								if(result.rows.length === 0) {
+									res.status(404).send({
+										message: 'Sensor not found'
+									});
+								} else {
+
+									// Send Result
+									res.status(200).send(result.rows[0]);
+								}
+							}
+						});
+					}
+				});
+			}
+		}
+	});
+};
