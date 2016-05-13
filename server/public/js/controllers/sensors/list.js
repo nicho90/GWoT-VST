2044c827b5fafeb622aa6e3040539bf8d413b724
rehans516 <rehans516@gmail.com>
2016-05-13 13:34:31 +0200

    Layout fixes

--- server/public/js/controllers/sensors/list.js
+++ server/public/js/controllers/sensors/list.js
@@ -2,7 +2,7 @@ var app = angular.module("gwot-vst");
 
 
 // LIST
-app.controller("ListSensorController", function($scope) {
+app.controller("SensorListController", function($scope) {
 
     // Init
     $scope.sensors = [];
