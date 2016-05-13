7ea942fa21a001887e3ecd7f604a5718d6f28fb1
rehans516 <rehans516@gmail.com>
2016-05-13 13:01:39 +0200

    created a nav bar controller

---
+++ server/public/js/controllers/navController.js
@@ -0,0 +1,20 @@
+var app = angular.module("gwot-vst");
+
+
+/**
+ * Navbar Controller
+ * @param  {[type]} "NavController"  [description]
+ * @param  {[type]} function($scope, $translate    [description]
+ * @return {[type]}                  [description]
+ */
+app.controller("NavController", function($scope, $translate,$location) {
+/**
+ * Highlight Menu Button if View is Active
+ * @param  {[type]} viewLocation [description]
+ * @return {[Boolean]}              [description]
+ */
+    $scope.isActive=function(viewLocation){
+      return viewLocation===$location.path();
+    };
+
+});
