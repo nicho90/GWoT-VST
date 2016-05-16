f76b245d767227d34c5c31d0ebd0e11bce44df61
Nicho <nicho90@web.de>
2016-05-17 00:30:56 +0200

    Implemented REST-API Endpoint /users/:username/thresholds (POST)
    Create a new threshold for a user

---
+++ server/controllers/thresholds/post.js
@@ -0,0 +1,91 @@
+var pg = require('pg');
+var types = require('pg').types;
+types.setTypeParser(1700, 'text', parseFloat);
+var _ = require('underscore');
+var jwt = require('jsonwebtoken');
+var secret = require('./../../config/secret');
+var db_settings = require('../../server.js').db_settings;
+var errors = require('./../../config/errors');
+var verifier = require('./../../config/verifier');
+
+var Ajv = require('ajv');
+var schema = require('./../../models/threshold');
+var ajv = Ajv({"format": "full"});
+var validate = ajv.compile(schema);
+
+
+// POST
+exports.request = function(req, res){
+
+	// Authentication
+	jwt.verify(req.headers.token, secret.key, function(err, decoded) {
+		if (err) {
+			res.status(errors.authentication.error_2.code).send(_.extend(errors.authentication.error_2, { err: err }));
+			return console.error(errors.authentication.error_2.message);
+        } else {
+
+			// Further token valiation
+			var validation = verifier(decoded, req.params.username);
+			if(!validation.success) {
+				res.status(errors.authentication.error_3.code).send(_.extend(errors.authentication.error_3, { message: validation.message }));
+				return console.error(errors.authentication.error_3.message);
+			} else {
+
+				// Schema Validation
+				var valid = validate(_.extend(req.body, { username: req.params.username} ));
+				if (!valid) {
+					res.status(errors.schema.error_1.code).send(_.extend(errors.schema.error_1, {
+						err: validate.errors[0].dataPath + ": " + validate.errors[0].message
+					}));
+					return console.error(validate.errors[0].dataPath + ": " + validate.errors[0].message);
+				} else {
+
+					// Create URL
+					var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+
+					// Connect to Database
+					pg.connect(url, function(err, client, done) {
+						if(err) {
+				            res.status(errors.database.error_1.code).send(errors.database.error_1);
+							return console.error(errors.database.error_1.message, err);
+						} else {
+
+							// Database Query
+							client.query("INSERT INTO Thresholds (created, updated, username, description, value, category) VALUES (now(), now(), $1, $2, $3, $4);",
+								[
+									req.params.username,
+									req.body.description,
+									req.body.value,
+									req.body.category
+								], function(err, result) {
+								done();
+
+								if(err) {
+									res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+				                    return console.error(errors.database.error_2.message, err);
+								} else {
+
+									// Database Query
+									client.query('SELECT * FROM thresholds WHERE username=$1 ORDER BY created DESC;', [
+										req.params.username
+									], function(err, result) {
+										done();
+
+					                    if(err) {
+											res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+						                    return console.error(errors.database.error_2.message, err);
+					                    } else {
+
+											// Send Result
+											res.status(201).send(result.rows[0]);
+										}
+									});
+								}
+							});
+						}
+					});
+				}
+			}
+		}
+	});
+};
