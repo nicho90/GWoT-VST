5d42ce600be31489e45317680c1846f217693b6a
Nicho <nicho90@web.de>
2016-05-17 00:30:07 +0200

    Implemented REST-API Endpoint /users/:username/thresholds/:threshold_id (PUT)
    Edit a threshold by its id and a username

---
+++ server/controllers/thresholds/put.js
@@ -0,0 +1,119 @@
+var pg = require('pg');
+var types = require('pg').types;
+types.setTypeParser(1700, 'text', parseFloat);
+var _ = require('underscore');
+var jwt = require('jsonwebtoken');
+var secret = require('./../../config/secret');
+var db_settings = require('../../server.js').db_settings;
+var errors = require('./../../config/errors');
+var verifier = require('./../../config/verifier');
+
+
+// PUT
+exports.request = function(req, res){
+
+	// TODO: Schema-Validation
+
+	// Decode Token
+	jwt.verify(req.headers.token, secret.key, function(err, decoded) {
+		if (err) {
+			res.status(401).json({
+				message: 'Failed to authenticate with this token'
+			});
+        } else {
+
+			// Further token valiation
+			var validation = verifier(decoded, req.params.username);
+			console.log(validation);
+
+			if(!validation.success) {
+				res.status(401).json({
+					message: validation.message
+				});
+			} else {
+
+				// Create URL
+				var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+
+				// Connect to Database
+				pg.connect(url, function(err, client, done) {
+					if(err) {
+						res.status(errors.database.error_1.code).send(errors.database.error_1);
+						return console.error(errors.database.error_1.message, err);
+					} else {
+
+						// Database Query
+						client.query('SELECT * FROM Thresholds WHERE username=$1 AND threshold_id=$2;', [
+							req.params.username,
+							req.params.threshold_id
+						], function(err, result) {
+							done();
+
+							if(err) {
+								res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+								return console.error(errors.database.error_2.message, err);
+							} else {
+
+								// Check if threshold exists
+								if(result.rows.length === 0) {
+									res.status(errors.query.error_5.code).send(errors.query.error_5);
+									return console.error(errors.query.error_5.message);
+								} else {
+
+									// Prepare Query
+									var query = "UPDATE Thresholds SET " +
+										"updated=now(), " +
+										"description=($3), " +
+										"value=($4), " +
+										"category=($5), " +
+										"WHERE username=$1 AND threshold_id=$2;";
+
+									// Database Query
+									client.query(query, [
+										req.params.username,
+										req.params.threshold_id,
+										req.body.description,
+										req.body.value,
+										req.body.category
+									], function(err, result) {
+										done();
+
+										if(err) {
+											res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+											return console.error(errors.database.error_2.message, err);
+										} else {
+
+											// Database Query
+											client.query('SELECT * FROM Thresholds WHERE username=$1 AND threshold_id=$2;', [
+												req.params.username,
+												req.params.threshold_id
+											], function(err, result) {
+												done();
+
+												if(err) {
+													res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+													return console.error(errors.database.error_2.message, err);
+												} else {
+
+													// Check if sensor exists
+													if(result.rows.length === 0) {
+														res.status(errors.query.error_5.code).send(errors.query.error_5);
+														return console.error(errors.query.error_5.message);
+													} else {
+
+														// Send Result
+														res.status(200).send(result.rows[0]);
+													}
+												}
+											});
+										}
+									});
+								}
+							}
+						});
+					}
+				});
+			}
+		}
+	});
+};
