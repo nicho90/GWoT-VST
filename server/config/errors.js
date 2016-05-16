558b5897a4b0477e46351d7516088f1fe9e3a7b7
Nicho <nicho90@web.de>
2016-05-16 22:42:36 +0200

    Added error-messages
    Better overview of all REST-API errors

---
+++ server/config/errors.js
@@ -0,0 +1,64 @@
+/**
+ * Error Messages
+ */
+module.exports = {
+    schema: {
+        error_1: {
+            code: 400,
+            message: 'Invalid schema'
+        }
+    },
+    database: {
+        error_1: {
+            code: 500,
+            message: 'Error fetching client from pool'
+        },
+        error_2: {
+            code: 401,
+            message: 'Error running query'
+        }
+    },
+    authentication: {
+        error_1: {
+            code: 401,
+            message: 'Wrong password'
+        },
+        error_2: {
+            code: 401,
+            message: 'Failed to authenticate with this token'
+        },
+        error_3: {
+            code: 401
+        }
+    },
+    query: {
+        error_1: {
+            code: 404,
+            message: 'User not found'
+        },
+        error_2: {
+            code: 404,
+            message: 'Sensor not found'
+        },
+        error_3: {
+            code: 404,
+            message: 'Measurement not found'
+        },
+        error_4: {
+            code: 404,
+            message: 'Threshold not found'
+        },
+        error_5: {
+            code: 404,
+            message: 'Subscription not found'
+        },
+        error_6: {
+            code: 404,
+            message: 'Time-Serie not found'
+        },
+        error_7: {
+            code: 404,
+            message: 'Vehicle not found'
+        }
+    }
+};
