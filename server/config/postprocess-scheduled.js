dc35cf1da4dc7a41081c3ada652d36766e08d6c2
Heinrich Löwen <loewen.heinrich@uni-muenster.de>
2016-05-19 22:21:03 +0200

    changes

--- server/config/postprocess-scheduled.js
+++ server/config/postprocess-scheduled.js
@@ -1,30 +1,32 @@
 var pg = require('pg');
 var db_settings = require('../server.js').db_settings;
-var waterfall = require('async-waterfall');
-var errors = require('./../../config/errors');
+var async = require('async');
+var errors = require('./errors');
 
 /**
  * Postprocess Observations from the Scheduled topic
  */
 exports.process = function(message) {
-  var measurements = JSON.parse(message).features;
-
-  var measurement = median(measurements); //Select the median measurement
-  console.log("Half: ", measurement);
-
   // Create URL
-  waterfall([
-    function(callback){
-      if(checkDeviceID(measurement.properties.device_id)){
-        callback(null, true);
-      } else {
-        callback(null, false);
-      };
-    },
-    function(sensor, callback) {
-      console.log("Second callback ", sensor);
-      callback(null);
-    }
+  async.waterfall([
+      function(callback) {
+        var measurements = JSON.parse(message).features;
+        var measurement = median(measurements); //Select the median measurement
+        console.log("Half: ", measurement);
+        callback(null, measurement);
+      },
+      function(measurement, callback) {
+        if (checkDeviceID(measurement.properties.device_id)) {
+          callback(null, measurement, true);
+        } else {
+          //TODO abbruch
+          callback(null, measurement, false);
+        };
+      },
+      function(measurement, sensor, callback) {
+        console.log("Second callback ", measurement, sensor);
+        callback(null);
+      }
     ], function(callback) {
       console.log("waterfall callback");
     }
@@ -42,33 +44,30 @@ var median = function(values) {
   return values[half];
 };
 
-var checkDeviceID = function(id) {
+var checkDeviceID = function(device_id) {
   var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
 
   // TODO match device_id with sensors-table
   pg.connect(url, function(err, client, done) {
-    if (err) {
-      res.status(errors.database.error_1.code).send(errors.database.error_1);
-      return console.error(errors.database.error_1.message, err);
-    } else {
-      // Database query
-      client.query('SELECT device_id FROM Sensors;', function(err, result) {
-        done();
-        if (err) {
-          return console.error(errors.database.error_2.message, err);
-        } else {
-          console.log("Database resp.: ", result.rows);
-          var obj = result.rows.filter(function(val) {
-            return val.device_id === id;
-          })[0];
-
-          if (obj) {
-            return true;
-          } else {
-            return false;
+      if (err) {
+        res.status(errors.database.error_1.code).send(errors.database.error_1);
+        return console.error(errors.database.error_1.message, err);
+      } else {
+        // Database query
+        client.query('SELECT device_id FROM Sensors WHERE device_id=$1;', [device_id], function(err, result) {
+            done();
+            if (err) {
+              return console.error(errors.database.error_2.message, err);
+            } else {
+              console.log("Database resp.: ", result.rows);
+              if (result.rows.length < 0) {
+                return true;
+              } else {
+                return false;
+              }
+            }
           }
-        }
-      });
+        });
     }
   });
 };
