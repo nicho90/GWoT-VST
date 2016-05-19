9ce4edb6dc416a2b18732801914e90b5ae326158
Heinrich Löwen <loewen.heinrich@uni-muenster.de>
2016-05-19 23:03:17 +0200

    changes

--- server/config/postprocess-scheduled.js
+++ server/config/postprocess-scheduled.js
@@ -3,61 +3,82 @@ var db_settings = require('../server.js').db_settings;
 var async = require('async');
 var errors = require('./errors');
 
+
 /**
  * Postprocess Observations from the Scheduled topic
  */
 exports.process = function(message) {
 
-    // Start pipeline
-    async.waterfall([
+  // Create URL
+  var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
 
-            function(callback) {
-                var measurements = JSON.parse(message).features;
-                var measurement = median(measurements); //Select the median measurement
-                //console.log("Half: ", measurement);
-                callback(null, measurement);
-            },
-            function(measurement, callback) {
-                var device_id = measurement.properties.device_id;
-                // Create URL
-                var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+  // Connect to Database
+  pg.connect(url, function(err, client, done) {
 
-                // TODO match device_id with sensors-table
-                pg.connect(url, function(err, client, done) {
-                    if (err) {
-                        return console.error(errors.database.error_1.message, err);
-                    } else {
-                        // Database query
-                        client.query('SELECT sensor_id, device_id FROM Sensors WHERE device_id=$1;', [device_id], function(err, result) {
-                            done();
-                            if (err) {
-                                return console.error(errors.database.error_2.message, err);
-                            } else {
-                                console.log("Database resp.: ", result.rows);
-                                if (result.rows.length > 0) {
+    if (err) {
+      return console.error(errors.database.error_1.message, err);
+    } else {
 
-                                    console.log(result.rows[0].sensor_id);
+      // Start pipeline
+      async.waterfall([
 
-                                    callback(null, measurement, result.rows[0].sensor_id);
-                                } else {
-                                    callback(null, measurement, null);
-                                }
-                            }
-                        });
-                    }
-                });
-            },
-            function(measurement, sensor, callback) {
-                console.log("Second callback ", measurement, sensor);
-                callback(null);
+        // Calculate Median
+        function(callback) {
+          var measurements = JSON.parse(message).features;
+          var measurement = median(measurements); //Select the median measurement
+          //console.log("Half: ", measurement);
+          callback(null, measurement);
+        },
+
+        // Find sensor_id in Sensors with device_id
+        function(measurement, callback) {
+
+          // Database query
+          client.query('SELECT sensor_id, device_id FROM Sensors WHERE device_id=$1;', [
+            measurement.properties.device_id
+          ], function(err, result) {
+            done();
+
+            if (err) {
+              return console.error(errors.database.error_2.message, err);
+            } else {
+              if (result.rows.length > 0) {
+
+                console.log(result.rows[0].sensor_id);
+
+                callback(null, measurement, result.rows[0].sensor_id);
+              } else {
+
+                // TODO Abbruch
+                callback(null, measurement, null);
+              }
             }
-        ], function(callback) {
-            console.log("waterfall callback");
+          });
+        },
+        function(measurement, sensor_id, callback) {
+
+          console.log("Second callback ", measurement, sensor_id);
+
+          // Database query
+          /*client.query('INSERT INTO Measurements (created, updated, sensor_id, distance, measured) VALUES (now(), now(), $1, $2, now());', [
+            sensor_id,
+            measurement.distance //,
+            // TODO: Use measurement.measured instead of now()
+          ], function(err, result) {
+            done();
+
+            if (err) {
+              return console.error(errors.database.error_2.message, err);
+            } else {
+              callback(null, sensor_id);
+            }
+          });*/
         }
-
-    );
-
-    // TODO push measurment to measurments-table
+      ], function(callback) {
+        console.log("waterfall callback");
+      });
+    }
+  });
 };
 
 
@@ -67,9 +88,9 @@ exports.process = function(message) {
  * @return {[type]}        [description]
  */
 var median = function(values) {
-    values.sort(function(a, b) {
-        return a.properties.distance.value - b.properties.distance.value;
-    });
-    var half = Math.floor(values.length / 2);
-    return values[half];
+  values.sort(function(a, b) {
+    return a.properties.distance.value - b.properties.distance.value;
+  });
+  var half = Math.floor(values.length / 2);
+  return values[half];
 };
