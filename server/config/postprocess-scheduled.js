f7946f445ba5df1a0507826e735b05922bbb4697
Heinrich Löwen <loewen.heinrich@uni-muenster.de>
2016-05-19 21:03:32 +0200

    postprocess check device_id

--- server/config/postprocess-scheduled.js
+++ server/config/postprocess-scheduled.js
@@ -1,5 +1,5 @@
 var pg = require('pg');
-var db_settings = require('../../server.js').db_settings;
+var db_settings = require('../server.js').db_settings;
 
 /**
  * Postprocess Observations from the Scheduled topic
@@ -7,10 +7,28 @@ var db_settings = require('../../server.js').db_settings;
 exports.process = function(message) {
   var measurements = JSON.parse(message).features;
 
-  var medianMeasurement = median(measurements); //Select the median measurement
-  console.log("Half: ", medianMeasurement);
+  var measurement = median(measurements); //Select the median measurement
+  console.log("Half: ", measurement);
 
   // Create URL
+  if (checkDeviceID(measurement.properties.device_id)) {
+    console.log("Device_ID matches. Proceed...");
+  } else {
+    console.log("Device_ID does not match.");
+  }
+
+  // TODO push measurment to measurments-table
+};
+
+var median = function(values) {
+    values.sort(function(a, b) {
+        return a.properties.distance.value - b.properties.distance.value;
+    });
+    var half = Math.floor(values.length / 2);
+    return values[half];
+};
+
+var checkDeviceID = function(id) {
   var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
 
   // TODO match device_id with sensors-table
@@ -25,19 +43,12 @@ exports.process = function(message) {
               if (err) {
                   return console.error(errors.database.error_2.message, err);
               } else {
-                  console.log("Database resp.: ", result.rows)
+                  console.log("Database resp.: ", result.rows);
+                  result.rows.filter(function(obj) {
+                    console.log(obj.device_id === id);
+                  })
               }
           });
       }
   });
-  
-  // TODO push measurment to measurments-table
-};
-
-var median = function(values) {
-    values.sort(function(a, b) {
-        return a.properties.distance.value - b.properties.distance.value;
-    });
-    var half = Math.floor(values.length / 2);
-    return values[half];
 };
