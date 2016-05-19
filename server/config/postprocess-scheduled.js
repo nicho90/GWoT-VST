d2addc2cd6ff345c11c54f121322eba648846f7e
Heinrich Löwen <loewen.heinrich@uni-muenster.de>
2016-05-19 22:35:32 +0200

    changes

--- server/config/postprocess-scheduled.js
+++ server/config/postprocess-scheduled.js
@@ -12,15 +12,19 @@ exports.process = function(message) {
       function(callback) {
         var measurements = JSON.parse(message).features;
         var measurement = median(measurements); //Select the median measurement
-        console.log("Half: ", measurement);
+        //console.log("Half: ", measurement);
         callback(null, measurement);
       },
       function(measurement, callback) {
-        if (checkDeviceID(measurement.properties.device_id)) {
-          callback(null, measurement, true);
+        var sensor_id = checkDeviceID(measurement.properties.device_id);
+        console.log(sensor_id);
+        if (sensor_id) {
+          console.log("Callback true");
+          callback(null, measurement, sensor_id);
         } else {
           //TODO abbruch
-          callback(null, measurement, false);
+          console.log("Callback false");
+          callback(null, measurement, null);
         };
       },
       function(measurement, sensor, callback) {
@@ -50,22 +54,21 @@ var checkDeviceID = function(device_id) {
   // TODO match device_id with sensors-table
   pg.connect(url, function(err, client, done) {
       if (err) {
-        res.status(errors.database.error_1.code).send(errors.database.error_1);
         return console.error(errors.database.error_1.message, err);
       } else {
         // Database query
-        client.query('SELECT device_id FROM Sensors WHERE device_id=$1;', [device_id], function(err, result) {
+        client.query('SELECT sensor_id, device_id FROM Sensors WHERE device_id=$1;', [device_id], function(err, result) {
             done();
             if (err) {
               return console.error(errors.database.error_2.message, err);
             } else {
               console.log("Database resp.: ", result.rows);
-              if (result.rows.length < 0) {
-                return true;
+              if (result.rows.length > 0) {
+                return result.rows[0].sensor_id;
               } else {
-                return false;
+                return null;
               }
-            }  
+            }
         });
     }
   });
