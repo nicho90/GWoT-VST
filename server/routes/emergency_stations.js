ae0fb0b137bac8f5f984ba30bdc4841fcd372fc1
Nicho <nicho90@web.de>
2016-05-19 20:04:42 +0200

    Updated routes
    for missing REST-API endpoints

--- server/routes/emergency_stations.js
+++ server/routes/emergency_stations.js
@@ -5,8 +5,11 @@ var list = require('../controllers/emergency_stations/list');
 var get = require('../controllers/emergency_stations/get');
 
 
-// LIST
+// LIST (all or query nearest Emergency Stations by longitude & latitude)
 router.get('/emergency_stations', list.request);
+
+// LIST (nearest Emergency Stations of a Sensor)
+router.get('/sensors/:sensor_id/emergency_stations', list.request);
 
 // GET
 router.get('/emergency_stations/:emergency_station_id', get.request);
--- server/routes/service_stations.js
+++ server/routes/service_stations.js
@@ -5,8 +5,11 @@ var list = require('../controllers/service_stations/list');
 var get = require('../controllers/service_stations/get');
 
 
-// LIST
+// LIST (all or query nearest Service Stations by longitude & latitude)
 router.get('/service_stations', list.request);
+
+// LIST (nearest Service Stations of a Sensor)
+router.get('/sensors/:sensor_id/service_stations', list.request);
 
 // GET
 router.get('/service_stations/:service_station_id', get.request);
