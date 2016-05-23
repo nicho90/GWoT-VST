1b46b7c40f6190ec3728ca1468b45fbd64d64501
Nicho <nicho90@web.de>
2016-05-23 21:48:06 +0200

    Added emergency_station-model

---
+++ server/models/emergency_station.js
@@ -0,0 +1,56 @@
+/**
+ * Emergency-Station Model for schema validation
+ * @type {Object}
+ */
+module.exports = {
+    "properties": {
+        "name": {
+            "type": "string",
+            "minLength": 1
+        },
+        "phone_number": {
+            "type": "string",
+            "minLength": 1
+        },
+        "street": {
+            "type": "string",
+            "minLength": 1
+        },
+        "house_number": {
+            "type": "string"
+        },
+        "addition": {
+            "type": "string"
+        },
+        "zip_code": {
+            "type": "string",
+            "minLength": 1
+        },
+        "city": {
+            "type": "string",
+            "minLength": 1
+        },
+        "country": {
+            "type": "string",
+            "minLength": 1
+        },
+        "lng": {
+            "type": "number",
+            "minimum": 0
+        },
+        "lat": {
+            "type": "number",
+            "minimum": 0
+        }
+    },
+    "required": [
+        "name",
+        "phone_number",
+        "street",
+        "zip_code",
+        "city",
+        "country",
+        "lng",
+        "lat"
+    ]
+};
