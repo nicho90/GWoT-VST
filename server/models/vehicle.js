142a0f0e3ccdced83469ee706a44096b76ad497f
Nicho <nicho90@web.de>
2016-05-23 21:47:22 +0200

    Updated vehicle-model

--- server/models/vehicle.js
+++ server/models/vehicle.js
@@ -4,12 +4,45 @@
  */
 module.exports = {
     "properties": {
-        "description": {
+        "brand": {
             "type": "string",
             "minLength": 1
+        },
+        "name": {
+            "type": "string",
+            "minLength": 1
+        },
+        "year": {
+            "type": "integer",
+            "minimum": 0
+        },
+        "warning_height": {
+            "type": "number",
+            "minimum": 0
+        },
+        "critical_height": {
+            "type": "number",
+            "minimum": 0
+        },
+        "category": {
+            "type": "string",
+            "enum": [
+                "BIKE",
+                "WHEELCHAIR",
+                "SCOOTER",
+                "MOTORBIKE",
+                "CAR",
+                "BUS",
+                "TRUCK"
+            ]
         }
     },
     "required": [
-        "description"
+        "brand",
+        "name",
+        "year",
+        "warning_height",
+        "critical_height",
+        "category"
     ]
 };
