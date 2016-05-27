c6e66b834382232ce758a67d824e8fb92f3d5f8e
Nicho <nicho90@web.de>
2016-05-27 10:41:04 +0200

    Implemented User-Registration for WebClient
    - Added registration-view (templating)
    - Added userCreateController for registration-view
    - Updated Route Provider for registration
    - Added userService for REST-API-Connection (sign up with new
    user-profile)

--- server/public/js/modules/routes.js
+++ server/public/js/modules/routes.js
@@ -1,32 +1,39 @@
-/**
- * Created by timmimim on 13/05/16.
- */
-
 var app = angular.module("routes", []);
 
 /**
  * Route Provider
- * @param   {[type]}    funtion($routeProvider  [description]
- * @return  {[type]}                            [description]
  */
 app.config(function($routeProvider) {
     $routeProvider
-    
-        //HOME (LOGIN)
+
+        // Home (Map)
         .when("/", {
             templateUrl: "/js/templates/home.html",
             controller: "HomeController"
         })
-            
-            
-        //SENSORS
+
+
+        // Sensors
         .when("/sensors", {
             templateUrl: "/js/templates/sensors/list.html",
             controller: "SensorListController"
         })
-    
+
+
+        // Users
+        .when("/new/user", {
+            templateUrl: "/js/templates/users/create.html",
+            controller: "UserCreateController"
+        })
+        .when("/users/:username", {
+            templateUrl: "/js/templates/users/details.html",
+            controller: "UserDetailsController"
+        })
+
+
+        // Redirect
         .otherwise({
             redirectTo: "/"
         });
-    
-})
+
+});
---
+++ server/public/js/services/userService.js
@@ -0,0 +1,39 @@
+var app = angular.module("userService", []);
+
+/**
+ * User Service Provider
+ */
+app.factory('$userService', function($http, config) {
+
+    return {
+        getDefault: function() {
+            return {
+                username : "",
+                password : "",
+                first_name : "",
+                last_name : "",
+                language : "en_US"
+            };
+        },
+        create: function(data){
+            return $http.post(config.apiURL + "/users", data);
+        },
+        get: function(token, username){
+            return $http.get(config.apiURL + "/users/" + username, {
+                headers: { 'token': token }
+            });
+        },
+        edit: function(token, username, data){
+            return $http.put(config.apiURL + "/users/" + username, {
+                headers: { 'token': token },
+                body: data
+            });
+        },
+        delete: function(token, username){
+            return $http.delete(config.apiURL + "/users/" + username, {
+                headers: { 'token': token }
+            });
+        }
+    };
+
+});
