221677654a05877a988834030fe761407718adfb
Heinrich Löwen <loewen.heinrich@gmail.com>
2016-05-29 09:40:06 +0200

    bugfix postprocessing realtime

--- rpi/mqtt/mqtt-pub-test.js
+++ rpi/mqtt/mqtt-pub-test.js
@@ -4,7 +4,7 @@ var client  = mqtt.connect('mqtt://giv-gwot-vst.uni-muenster.de:1883');
 client.on('connect', function () {
   //client.publish('/data/realtime', '{"device_id": "rpi-1","status" : true}', {retain: false, qa: 1});
   //client.publish('/data/realtime', '{"device_id": "rpi-1","status" : false}', {retain: false, qa: 1});
-  client.publish('/settings', '{"device_id": "rpi-1","interval": 10000}', {retain: false, qa: 1});
+  client.publish('/settings', '{"device_id": "rpi-1","interval": 60000}', {retain: false, qa: 1});
 
   client.end();
 });
--- server/config/postprocess-realtime.js
+++ server/config/postprocess-realtime.js
@@ -18,51 +18,64 @@ exports.process = function(message) {
     // Start pipeline
     async.waterfall([
 
-        // 1. Find sensor_id in Sensors with device_id
-        function(callback) {
-            // Parse measurement
-            var measurement = JSON.parse(message).features[0];
+            // 1. Find sensor_id in Sensors with device_id
+            function(callback) {
+                // Parse measurement
+                var measurement = JSON.parse(message).features[0];
 
-            // Database query
-            client.query('SELECT * FROM Sensors WHERE device_id=$1;', [
-                measurement.properties.device_id
-            ], function(err, result) {
-                done();
+                // Create URL
+                var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
 
-                if (err) {
-                    console.error(errors.database.error_2.message, err);
-                    callback(new Error(errors.database.error_2.message));
-                } else {
-                    if (result.rows.length > 0) {
-                        callback(null, measurement, result.rows[0]);
+                // Connect to Database
+                pg.connect(url, function(err, client, done) {
+
+                    if (err) {
+                        return console.error(errors.database.error_1.message, err);
                     } else {
-                        console.error(errors.query.error_2.message);
-                        callback(new Error(errors.query.error_2.message));
+
+                        // Database query
+                        client.query('SELECT * FROM Sensors WHERE device_id=$1;', [
+                            measurement.properties.device_id
+                        ], function(err, result) {
+                            done();
+
+                            if (err) {
+                                console.error(errors.database.error_2.message, err);
+                                callback(new Error(errors.database.error_2.message));
+                            } else {
+                                if (result.rows.length > 0) {
+                                    callback(null, measurement, result.rows[0]);
+                                } else {
+                                    console.error(errors.query.error_2.message);
+                                    callback(new Error(errors.query.error_2.message));
+                                }
+                            }
+                        });
                     }
+                });
+            },
+
+            // 2. Reject measurement if measured distance > sensor height
+            function(measurement, sensor, callback) {
+                if (measurement.properties.distance.value > sensor.sensor_height) {
+                    callback(new Error(errors.measurement.error_1.message));
+                } else {
+                    callback(null, measurement);
                 }
-            });
-        },
+            },
 
-        // 2. Reject measurement if measured distance > sensor height
-        function(measurement, sensor, callback) {
-            if (measurement.properties.distance.value > sensor.sensor_height) {
-                callback(new Error(errors.measurement.error_1.message));
-            } else {
-                callback(null, measurement);
+            // 3. Forward message via websockets
+            function(measurement, callback) {
+                //TODO
+                callback();
             }
-        },
 
-        // 3. Forward message via websockets
-        function(measurement, callback) {
-            //TODO
-            callback();
-        }
-
-    ], function(err, callback) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Pipeline has been finished! " + new Date());
-        }
-    });
+        ],
+        function(err, callback) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Pipeline has been finished! " + new Date());
+            }
+        });
 };
