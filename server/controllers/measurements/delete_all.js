99592faf8e49868c650e837dc0a96fcd0e66a2b3
Heinrich Löwen <loewen.heinrich@gmail.com>
2016-05-31 17:43:29 +0200

    beautify

--- server/controllers/measurements/delete_all.js
+++ server/controllers/measurements/delete_all.js
@@ -10,54 +10,54 @@ var verifier = require('./../../config/verifier');
 
 
 // DELETE ALL
-exports.request = function(req, res){
+exports.request = function(req, res) {
 
-	// Decode Token
-	jwt.verify(req.headers.token, secret.key, function(err, decoded) {
-		if (err) {
-			res.status(401).json({
-				message: 'Failed to authenticate with this token'
-			});
+    // Decode Token
+    jwt.verify(req.headers.token, secret.key, function(err, decoded) {
+        if (err) {
+            res.status(401).json({
+                message: 'Failed to authenticate with this token'
+            });
         } else {
 
-			// Further token valiation
-			var validation = verifier(decoded, req.params.username);
-			console.log(validation);
+            // Further token valiation
+            var validation = verifier(decoded, req.params.username);
+            console.log(validation);
 
-			if(!validation.success) {
-				res.status(401).json({
-					message: validation.message
-				});
-			} else {
+            if (!validation.success) {
+                res.status(401).json({
+                    message: validation.message
+                });
+            } else {
 
-				// Create URL
-				var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+                // Create URL
+                var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
 
-				// Connect to Database
-				pg.connect(url, function(err, client, done) {
-					if(err) {
-						res.status(errors.database.error_1.code).send(errors.database.error_1);
-						return console.error(errors.database.error_1.message, err);
-					} else {
+                // Connect to Database
+                pg.connect(url, function(err, client, done) {
+                    if (err) {
+                        res.status(errors.database.error_1.code).send(errors.database.error_1);
+                        return console.error(errors.database.error_1.message, err);
+                    } else {
 
-						// Database Query
-						client.query("DELETE FROM Measurements WHERE sensor_id=$1;", [
-							req.params.sensor_id
-						], function(err, result) {
-							done();
+                        // Database Query
+                        client.query("DELETE FROM Measurements WHERE sensor_id=$1;", [
+                            req.params.sensor_id
+                        ], function(err, result) {
+                            done();
 
-							if(err) {
-								res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
-								return console.error(errors.database.error_2.message, err);
-							} else {
+                            if (err) {
+                                res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+                                return console.error(errors.database.error_2.message, err);
+                            } else {
 
-								// Send Result
-								res.status(204).send();
-							}
-						});
-					}
-				});
-			}
-		}
-	});
+                                // Send Result
+                                res.status(204).send();
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    });
 };
--- server/controllers/measurements/list.js
+++ server/controllers/measurements/list.js
@@ -10,54 +10,54 @@ var verifier = require('./../../config/verifier');
 
 
 // LIST
-exports.request = function(req, res){
+exports.request = function(req, res) {
 
-	// Create URL
-	var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+    // Create URL
+    var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
 
-	// Connect to Database
-	pg.connect(url, function(err, client, done) {
-		if(err) {
-			res.status(errors.database.error_1.code).send(errors.database.error_1);
-			return console.error(errors.database.error_1.message, err);
-		} else {
+    // Connect to Database
+    pg.connect(url, function(err, client, done) {
+        if (err) {
+            res.status(errors.database.error_1.code).send(errors.database.error_1);
+            return console.error(errors.database.error_1.message, err);
+        } else {
 
-			var query;
-			var singleData = false;
-			if(req.query.latest){
-				query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY measurement_timestamp DESC LIMIT 1;";
-				singleData = true;
-			} else if(req.query.maximum) {
-				query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY distance DESC LIMIT 1;";
-				singleData = true;
-			} else if(req.query.minimum) {
-				query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY distance ASC LIMIT 1;";
-				singleData = true;
-			} else {
-				query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY measurement_timestamp DESC LIMIT 1000;";
-			}
+            var query;
+            var singleData = false;
+            if (req.query.latest) {
+                query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY measurement_timestamp DESC LIMIT 1;";
+                singleData = true;
+            } else if (req.query.maximum) {
+                query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY distance DESC LIMIT 1;";
+                singleData = true;
+            } else if (req.query.minimum) {
+                query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY distance ASC LIMIT 1;";
+                singleData = true;
+            } else {
+                query = "SELECT * FROM Measurements WHERE sensor_id=$1 ORDER BY measurement_timestamp DESC LIMIT 1000;";
+            }
 
-			// Database Query
+            // Database Query
             client.query(query, [
-				req.params.sensor_id
-			], function(err, result) {
+                req.params.sensor_id
+            ], function(err, result) {
                 done();
 
-                if(err) {
-					res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
-					return console.error(errors.database.error_2.message, err);
+                if (err) {
+                    res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+                    return console.error(errors.database.error_2.message, err);
                 } else {
 
-					// Send Result
-					if(singleData){
-						res.status(200).send(result.rows[0]);
-					} else {
-						res.status(200).send(result.rows);
-					}
+                    // Send Result
+                    if (singleData) {
+                        res.status(200).send(result.rows[0]);
+                    } else {
+                        res.status(200).send(result.rows);
+                    }
 
                 }
             });
-		}
-	});
+        }
+    });
 
 };
