fae410e159d5109fe9571b876a50148d43a36cb5
Nicho <nicho90@web.de>
2016-06-11 22:41:34 +0200

    Revised REST-API-Endpoint /users/:username/sensors (POST)

--- server/routes/sensors.js
+++ server/routes/sensors.js
@@ -2,42 +2,57 @@ var express = require('express');
 var router = express.Router();
 
 var list = require('../controllers/sensors/list');
+//var post = require('../controllers/sensors/post');
+//var get = require('../controllers/sensors/get');
+//var put = require('../controllers/sensors/put');
+//var del = require('../controllers/sensors/delete');
+//var delete_all = require('../controllers/sensors/delete_all');
+
+
 var list_user = require('../controllers/sensors/list_user');
-var post = require('../controllers/sensors/post');
-var get_public = require('../controllers/sensors/get_public');
-var get_private = require('../controllers/sensors/get_private');
-//var put_public = require('../controllers/sensors/put_public');
-var put_private = require('../controllers/sensors/put_private');
-//var delete_public = require('../controllers/sensors/delete_public');
-var delete_private = require('../controllers/sensors/delete_private');
+var post_user = require('../controllers/sensors/post_user');
+//var get_user = require('../controllers/sensors/get_user');
+//var put_user = require('../controllers/sensors/put_user');
+//var delete_user = require('../controllers/sensors/delete_user');
+//var delete_all_user = require('../controllers/sensors/delete_all_user');
 
 
 // LIST
 router.get('/sensors', list.request);
 
+// POST (only admin)
+//router.post('/sensors', post.request);
+
+// DELETE ALL (only admin)
+//router.delete('/sensors', delete_all.request);
+
+// GET (only admin)
+// router.get('/sensors/:sensor_id', get.request);
+
+// PUT (only admin)
+// router.get('/sensors/:sensor_id', put.request);
+
+// DELETE (only admin)
+// router.get('/sensors/:sensor_id', del.request);
+
+
 // LIST (only created sensors of the user)
 router.get('/users/:username/sensors', list_user.request);
 
+// DELETE ALL (only created sensors of the user)
+// router.delete('/users/:username/sensors', delete_all_user.request);
+
 // POST
-router.post('/users/:username/sensors', post.request);
+router.post('/users/:username/sensors', post_user.request);
 
-// GET (public sensors)
-router.get('/sensors/:sensor_id', get_public.request);
+// GET
+// router.get('/users/:username/sensors/:sensor_id', get_user.request);
 
-// GET (only private sensors)
-router.get('/users/:username/sensors/:sensor_id', get_private.request);
+// PUT
+// router.put('/users/:username/sensors/:sensor_id', put_user.request);
 
-// PUT (only admin)
-//router.put('/sensors/:sensor_id', put_public.request);
-
-// PUT (only private sensors)
-router.put('/users/:username/sensors/:sensor_id', put_private.request);
-
-// DELETE (only admin)
-//router.delete('/sensors/:sensor_id', delete_public.request);
-
-// DELETE (only private sensors)
-router.delete('/users/:username/sensors/:sensor_id', delete_private.request);
+// DELETE
+// router.delete('/users/:username/sensors/:sensor_id', delete_user.request);
 
 
 
