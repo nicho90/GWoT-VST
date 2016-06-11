324157eb334c4e7c894bb21f11ace8b224b2803c
Nicho <nicho90@web.de>
2016-06-11 17:39:52 +0200

    Added missing measurementControllers

---
+++ server/controllers/measurements/get.js
@@ -0,0 +1,42 @@
+var pg = require('pg');
+var types = require('pg').types;
+types.setTypeParser(1700, 'text', parseFloat);
+var _ = require('underscore');
+var jwt = require('jsonwebtoken');
+var secret = require('./../../config/secret');
+var db_settings = require('../../server.js').db_settings;
+var errors = require('./../../config/errors');
+var verifier = require('./../../config/verifier');
+
+
+// GET
+exports.request = function(req, res) {
+
+    // Create URL
+    var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+
+    // Connect to Database
+    pg.connect(url, function(err, client, done) {
+        if (err) {
+            res.status(errors.database.error_1.code).send(errors.database.error_1);
+            return console.error(errors.database.error_1.message, err);
+        } else {
+
+            // Database query
+            client.query('SELECT * FROM Measurements WHERE measurement_id=$1;', [
+                req.params.measurement_id
+            ], function(err, result) {
+                done();
+
+                if (err) {
+                    res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+                    return console.error(errors.database.error_2.message, err);
+                } else {
+
+                    // Send Result
+                    res.status(200).send(result.rows[0]);
+                }
+            });
+        }
+    });
+};
--- server/routes/measurements.js
+++ server/routes/measurements.js
@@ -2,14 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 var list = require('../controllers/measurements/list');
-//var delete_all = require('../controllers/measurements/delete_all');
+var get = require('../controllers/measurements/get');
+var post = require('../controllers/measurements/post');
+var put = require('../controllers/measurements/put');
+var del = require('../controllers/measurements/delete');
+var delete_all = require('../controllers/measurements/delete_all');
 
 
 // LIST
 router.get('/sensors/:sensor_id/measurements', list.request);
 
-// DELETE ALL (only admin)
-//router.delete('/sensors/:sensor_id/measurements', delete_all.request);
+// POST
+router.post('/sensors/:sensor_id/measurements', post.request);
+
+// GET
+router.get('/sensors/:sensor_id/measurements/:measurement_id', get.request);
+
+// PUT
+router.put('/sensors/:sensor_id/measurements/:measurement_id', put.request);
+
+// DELETE
+router.delete('/sensors/:sensor_id/measurements/:measurement_id', del.request);
+
+// DELETE ALL
+router.delete('/sensors/:sensor_id/measurements', delete_all.request);
 
 
 module.exports = router;
