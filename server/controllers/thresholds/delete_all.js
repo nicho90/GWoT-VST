bbfc9684df8fa6714d9043e127c78cf05743c915
Nicho <nicho90@web.de>
2016-06-17 16:56:37 +0200

    Revised REST-API-Endpoint /users/:username/thresholds (DELETE)
    with new validation steps (to check if a User or Admin was
    authenticated)

--- server/controllers/thresholds/delete_all.js
+++ server/controllers/thresholds/delete_all.js
@@ -4,57 +4,82 @@ types.setTypeParser(1700, 'text', parseFloat);
 var _ = require('underscore');
 var jwt = require('jsonwebtoken');
 var secret = require('./../../config/secret');
+var db = require('./../../config/db');
 var db_settings = require('../../server.js').db_settings;
 var errors = require('./../../config/errors');
-var verifier = require('./../../config/verifier');
 
 
 // DELETE ALL
 exports.request = function(req, res){
 
-	// Decode Token
-	jwt.verify(req.headers.token, secret.key, function(err, decoded) {
-		if (err) {
-			res.status(401).json({
-				message: 'Failed to authenticate with this token'
-			});
-        } else {
+	// Create URL
+	var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
 
-			// Further token valiation
-			var validation = verifier(decoded, req.params.username);
-			console.log(validation);
+	// Connect to Database
+	pg.connect(url, function(err, client, done) {
+		if(err) {
+			res.status(errors.database.error_1.code).send(errors.database.error_1);
+			return console.error(errors.database.error_1.message, err);
+		} else {
 
-			if(!validation.success) {
-				res.status(401).json({
-					message: validation.message
-				});
+			// Check if User was authenticated
+			if(!req.headers.authorization || req.headers.authorization === ""){
+				res.status(errors.authentication.error_3.code).send(errors.authentication.error_3);
+				return console.error(errors.authentication.error_3.message);
 			} else {
 
-				// Create URL
-				var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+				// Decode Token
+				jwt.verify(req.headers.authorization, secret.key, function(err, decoded) {
+					if (err) {
+						res.status(errors.authentication.error_2.code).send(errors.authentication.error_2);
+						return console.error(errors.authentication.error_2.message);
+			        } else {
 
-				// Connect to Database
-				pg.connect(url, function(err, client, done) {
-					if(err) {
-						res.status(errors.database.error_1.code).send(errors.database.error_1);
-						return console.error(errors.database.error_1.message, err);
-					} else {
+						// Get username from authenticated user
+						var username = decoded.username;
 
-						// Database Query
-						client.query("DELETE FROM Thresholds WHERE username=$1;", [
-							req.params.username
-						], function(err, result) {
-							done();
+						// Check if authenticated user is the right user or admin
+						if(username === req.params.username || username === db.admin) {
 
-							if(err) {
-								res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
-								return console.error(errors.database.error_2.message, err);
-							} else {
+							// Database Query
+			                client.query('SELECT * FROM Users WHERE username=$1;', [
+			                    req.params.username
+			                ], function(err, result) {
+			                    done();
 
-								// Send Result
-								res.status(204).send();
-							}
-						});
+			                    if(err) {
+			                        res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+			                        return console.error(errors.database.error_2.message, err);
+			                    } else {
+
+			                        // Check if User exists
+			                        if(result.rows.length === 0) {
+										res.status(errors.query.error_1.code).send(errors.query.error_1);
+										return console.error(errors.query.error_1.message);
+			                        } else {
+
+										// Database Query
+										client.query("DELETE FROM Thresholds WHERE creator=$1;", [
+											req.params.username
+										], function(err, result) {
+											done();
+
+											if(err) {
+												res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+												return console.error(errors.database.error_2.message, err);
+											} else {
+
+												// Send Result
+												res.status(204).send();
+											}
+										});
+									}
+								}
+							});
+						} else {
+							res.status(errors.authentication.error_2.code).send(errors.authentication.error_2);
+							return console.error(errors.authentication.error_2.message);
+						}
 					}
 				});
 			}
