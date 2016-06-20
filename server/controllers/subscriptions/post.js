91384c9566c64897f1884c893f45c02bfcdeeb81
Nicho <nicho90@web.de>
2016-06-20 21:05:51 +0200

    Revised REST-API-Endpoint /users/:username/subscriptions (POST)
    with new validation steps (to check if a User or Admin was
    authenticated)

--- server/controllers/subscriptions/post.js
+++ server/controllers/subscriptions/post.js
@@ -4,12 +4,12 @@ types.setTypeParser(1700, 'text', parseFloat);
 var _ = require('underscore');
 var jwt = require('jsonwebtoken');
 var secret = require('./../../config/secret');
+var db = require('./../../config/db');
 var db_settings = require('../../server.js').db_settings;
 var errors = require('./../../config/errors');
-var verifier = require('./../../config/verifier');
 
 var Ajv = require('ajv');
-var schema = require('./../../models/subscription');
+var schema = require('./../../models/threshold');
 var ajv = Ajv({"format": "full"});
 var validate = ajv.compile(schema);
 
@@ -17,73 +17,119 @@ var validate = ajv.compile(schema);
 // POST
 exports.request = function(req, res){
 
-	// Authentication
-	jwt.verify(req.headers.token, secret.key, function(err, decoded) {
-		if (err) {
-			res.status(errors.authentication.error_2.code).send(_.extend(errors.authentication.error_2, { err: err }));
-			return console.error(errors.authentication.error_2.message);
-        } else {
+	// TODO: Schema-Validation
+	// and check if Sensor was public or authenticated user was creator of private sensor or admin
 
-			// Further token valiation
-			var validation = verifier(decoded, req.params.username);
-			if(!validation.success) {
-				res.status(errors.authentication.error_3.code).send(_.extend(errors.authentication.error_3, { message: validation.message }));
+	// Create URL
+	var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+
+	// Connect to Database
+	pg.connect(url, function(err, client, done) {
+		if(err) {
+			res.status(errors.database.error_1.code).send(errors.database.error_1);
+			return console.error(errors.database.error_1.message, err);
+		} else {
+
+			// Check if User was authenticated
+			if(!req.headers.authorization || req.headers.authorization === ""){
+				res.status(errors.authentication.error_3.code).send(errors.authentication.error_3);
 				return console.error(errors.authentication.error_3.message);
 			} else {
 
-				// Schema Validation
-				var valid = validate(_.extend(req.body, { username: req.params.username} ));
-				if (!valid) {
-					res.status(errors.schema.error_1.code).send(_.extend(errors.schema.error_1, {
-						err: validate.errors[0].dataPath + ": " + validate.errors[0].message
-					}));
-					return console.error(validate.errors[0].dataPath + ": " + validate.errors[0].message);
-				} else {
+				// Decode Token
+				jwt.verify(req.headers.authorization, secret.key, function(err, decoded) {
+					if (err) {
+						res.status(errors.authentication.error_2.code).send(errors.authentication.error_2);
+						return console.error(errors.authentication.error_2.message);
+			        } else {
 
-					// Create URL
-					var url = "postgres://" + db_settings.user + ":" + db_settings.password + "@" + db_settings.host + ":" + db_settings.port + "/" + db_settings.database_name;
+						// Get username from authenticated user
+						var username = decoded.username;
 
-					// Connect to Database
-					pg.connect(url, function(err, client, done) {
-						if(err) {
-				            res.status(errors.database.error_1.code).send(errors.database.error_1);
-							return console.error(errors.database.error_1.message, err);
-						} else {
+						// Check if authenticated user is the right user or admin
+						if(username === req.params.username || username === db.admin) {
 
 							// Database Query
-							client.query("INSERT INTO Subscriptions (created, updated, username, sensor_id, threshold_id) VALUES (now(), now(), $1, $2, $3);",
-								[
-									req.params.username,
-									req.body.sensor_id,
-									req.body.threshold_id
-								], function(err, result) {
-								done();
+			                client.query("SELECT * FROM Users WHERE username=$1;", [
+			                    req.params.username
+			                ], function(err, result) {
+			                    done();
 
-								if(err) {
-									res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
-				                    return console.error(errors.database.error_2.message, err);
-								} else {
+			                    if(err) {
+			                        res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+			                        return console.error(errors.database.error_2.message, err);
+			                    } else {
 
-									// Database Query
-									client.query('SELECT * FROM Subscriptions WHERE username=$1 ORDER BY created DESC;', [
-										req.params.username
-									], function(err, result) {
-										done();
+			                        // Check if User exists
+			                        if(result.rows.length === 0) {
+										res.status(errors.query.error_1.code).send(errors.query.error_1);
+										return console.error(errors.query.error_1.message);
+			                        } else {
 
-					                    if(err) {
-											res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
-						                    return console.error(errors.database.error_2.message, err);
-					                    } else {
+										// Database Query
+										client.query("INSERT INTO Subscriptions (created, updated, creator, sensor_id, threshold_id, active) VALUES (now(), now(), $1, $2, $3, $4);",
+											[
+												req.params.username,
+												req.body.sensor_id,
+												req.body.threshold_id,
+												req.body.active
+											], function(err, result) {
+											done();
 
-											// Send Result
-											res.status(201).send(result.rows[0]);
-										}
-									});
+											if(err) {
+												res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+							                    return console.error(errors.database.error_2.message, err);
+											} else {
+
+												var query = "SELECT " +
+														"subscriptions.subscription_id, " +
+														"subscriptions.created, " +
+														"subscriptions.updated, " +
+														"subscriptions.creator, " +
+														"subscriptions.active, " +
+														"subscriptions.sensor_id, " +
+														"sensors.description AS sensor_description, " +
+														"subscriptions.threshold_id, " +
+														"thresholds.description AS threshold_description, " +
+														"thresholds.category AS threshold_category " +
+													"FROM Subscriptions subscriptions JOIN Sensors sensors ON subscriptions.sensor_id=sensors.sensor_id " +
+													"JOIN Thresholds thresholds ON subscriptions.threshold_id=thresholds.threshold_id "+
+													"WHERE subscriptions.creator=$1 " +
+													"ORDER BY created DESC;";
+
+												// Database Query
+												client.query(query, [
+													req.params.username
+												], function(err, result) {
+													done();
+
+								                    if(err) {
+														res.status(errors.database.error_2.code).send(_.extend(errors.database.error_2, err));
+									                    return console.error(errors.database.error_2.message, err);
+								                    } else {
+
+														// Check if Subscription exists
+								                        if(result.rows.length === 0) {
+															res.status(errors.query.error_5.code).send(errors.query.error_5);
+															return console.error(errors.query.error_5.message);
+								                        } else {
+
+															// Send Result
+															res.status(201).send(result.rows[0]);
+														}
+													}
+												});
+											}
+										});
+									}
 								}
 							});
+						} else {
+							res.status(errors.authentication.error_2.code).send(errors.authentication.error_2);
+							return console.error(errors.authentication.error_2.message);
 						}
-					});
-				}
+					}
+				});
 			}
 		}
 	});
