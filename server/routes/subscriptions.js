44ed535e1597a97a3f10d25fb3641208d8f71d6c
Nicho <nicho90@web.de>
2016-06-20 21:35:29 +0200

    Implemented  REST-API-Endpoint /sensors/:sensor_id/subscriptions (GET)
    to list all Subscriptions of a Sensor by its sensor_id, if the user is
    authenticated

--- server/routes/subscriptions.js
+++ server/routes/subscriptions.js
@@ -8,6 +8,8 @@ var put = require('../controllers/subscriptions/put');
 var del = require('../controllers/subscriptions/delete');
 var delete_all = require('../controllers/subscriptions/delete_all');
 
+var list_sensor = require('../controllers/subscriptions/list_sensor');
+
 
 // LIST
 router.get('/users/:username/subscriptions', list.request);
@@ -27,6 +29,9 @@ router.delete('/users/:username/subscriptions/:subscription_id', del.request);
 // DELETE ALL
 router.delete('/users/:username/subscriptions', delete_all.request);
 
+
+// LIST (Subscriptions of a User)
+router.get('/sensors/:sensor_id/subscriptions', list_sensor.request);
 
 
 module.exports = router;
